7351a590067729095a56a708315e6e8b6790a476
Te270 <79935865+Te270@users.noreply.github.com>
2021-11-17 00:19:23 +0200

    Базовые файлы

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { ToastContainer } from 'react-toastify';
+import './App.css';
+
+export default function App() {
+  return (
+    <div>
+      'Hello world!'
+      <ToastContainer />
+    </div>
+  );
+}
